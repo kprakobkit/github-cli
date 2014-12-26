bfa5c0ae34658c7b711c721527595aa44129de9d
Peter Prakobkit <kprakobkit@gmail.com>
2014-12-26 10:07:41 -0800

    completed retrival all emojis from github

---
+++ emojis.rb
@@ -0,0 +1,20 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
+
+uri = URI('https://api.github.com/emojis')
+
+response = Net::HTTP.start(uri.host, uri.port,
+  :use_ssl => uri.scheme == "https") do |http|
+  req = Net::HTTP::Get.new uri
+
+  http.request req
+end
+
+def print_emoji_names(json)
+  JSON.parse(json).keys.each { |name| puts name }
+end
+
+print_emoji_names(response.body)
+
