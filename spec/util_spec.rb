b089a81d464a22a6247eb9e119b3608a0a974f7f
Peter Prakobkit <kprakobkit@gmail.com>
2014-12-27 14:05:32 -0800

    implement test: Util#get_response should hit the right endpoint

--- spec/util_spec.rb
+++ spec/util_spec.rb
@@ -1,18 +1,23 @@
 require_relative '../util'
 require_relative '../user'
-require_relative '../Repo'
+require_relative '../repo'
 
 describe Util do
   let(:response) { {"name" => "user", "foo" => "bar" } }
   let(:endpoint) { 'users/kprakobkit' }
+  let(:repo) { double(watchers: 1) }
 
-  before :each do
-    http_response = double(body: response.to_json)
-    allow(Net::HTTP).to receive(:start).and_return(http_response)
-    @response = Util.get_response(endpoint)
+  it '#get_response should hit the right endpoint' do
+    allow(Repo).to receive(:new).and_return(repo)
+    allow(Util).to receive(:get_response).and_return(response)
+    User.find('kprakobkit')
+    expect(Util).to have_received(:get_response).with(endpoint)
   end
 
   it 'should return json' do
+    http_response = double(body: response.to_json)
+    allow(Net::HTTP).to receive(:start).and_return(http_response)
+    @response = Util.get_response(endpoint)
     expect(@response).to eq(response)
   end
 end
